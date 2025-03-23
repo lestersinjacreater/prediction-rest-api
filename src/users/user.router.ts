8ccd18278b0ac459086ff2d87063681d1ce0b247
lestersinjacreater <lestetsinjawatosia@gmail.com>
2025-03-23 08:51:22 +0300

    fixed users

--- src/users/user.router.ts
+++ src/users/user.router.ts
@@ -1,25 +1,49 @@
 import { Hono } from "hono";
-import {  getUser,getUsers, createUser, updateUser, deleteUser} from "./user.controller"
+import { 
+  getUser, 
+  getUsers, 
+  createUser, 
+  updateUser, 
+  deleteUser,
+  getUserByClerkId 
+} from "./user.controller";
 import { zValidator } from "@hono/zod-validator";
-import { userSchema } from "../validators";
-import { adminOrUserRoleAuth, adminRoleAuth} from "../middleware/bearAuth";
-// import { adminRoleAuth } from "../middleware/bearAuth";
+import { registerUserSchema, userSchema } from "../validators";
+import { adminOrUserRoleAuth, adminRoleAuth } from "../middleware/bearAuth";
+
 export const userRouter = new Hono();
 
-//get all users
-userRouter.get("/users",getUsers );
+// Get all users - admin only
+userRouter.get("/users", adminRoleAuth, getUsers);
 
-//get a single user   
-userRouter.get("/users/:id" , getUser)
-// create a user 
-userRouter.post("/users", zValidator('json', userSchema, (result, c) => {
+// Get a single user by numeric ID - accessible by admin or the user themselves
+userRouter.get("/users/:id", adminOrUserRoleAuth, getUser);
+
+// Get a single user by Clerk ID (for authenticated sessions) - accessible by admin or the user themselves
+userRouter.get("/users/clerk/:clerkId", adminOrUserRoleAuth, getUserByClerkId);
+
+// Create a user (registration using Clerk authentication - requires clerkId)
+userRouter.post(
+  "/users",
+  zValidator('json', registerUserSchema, (result, c) => {
     if (!result.success) {
-        return c.json(result.error, 400)
+      return c.json(result.error, 400);
     }
-}) ,createUser)
-//update a user
-userRouter.put("/users/:id",  updateUser)
+  }),
+  createUser
+);
 
-userRouter.delete("/users/:id", deleteUser)
-// search
-// userRouter.get("/users/search", searchUsers)
+// Update a user (using numeric ID, validated with userSchema) - accessible by admin or the user themselves
+userRouter.put(
+  "/users/:id",
+  adminOrUserRoleAuth,
+  zValidator('json', userSchema, (result, c) => {
+    if (!result.success) {
+      return c.json(result.error, 400);
+    }
+  }),
+  updateUser
+);
+
+// Delete a user (using numeric ID) - admin or the user themselves
+userRouter.delete("/users/:id", adminOrUserRoleAuth, deleteUser);
